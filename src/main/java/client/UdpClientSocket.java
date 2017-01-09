f4f82d8cc34290fcba67a5b0ab430e166d89f768
Yair Ganon <yairganon@gmai.com>
2017-01-10 00:19:33 +0200

    asdasdas

--- src/main/java/client/UdpClientSocket.java
+++ src/main/java/client/UdpClientSocket.java
@@ -37,7 +37,7 @@ public class UdpClientSocket {
         Offer offerRequest = new Offer(receivePacket.getData());
         System.out.println("FROM SERVER:" + offerRequest);
         clientSocket.close();
-        System.out.printf("here");
+        System.out.printf("hefffre");
         openTcpConnection(offerRequest);
     }
 
